7815a708185a91754007006c57478b4c1ba8e3e9
julieth <jcobosg@unbosque.edu.co>
2017-03-02 10:03:43 -0500

    Carga funciones

--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,28 @@ function capturar(){
     // obtenemos el valor de los elemento
     var xb = document.getElementById("xb").value;
     var xa = document.getElementById("xa").value;
-	resultado = xb+xa;
+	resultado = parseInt(xb)+parseInt(xa);
 	document.getElementById("resultado").value = resultado;
 } 
+
+function factorLorentz (){
+	var v = document.getElementById("v").value;
+	var c = 3 * 10^8;
+	var k = Math.pow(1-(v^2/c^2));
+	return k;
+}
+
+function tiempo (){
+	var v = document.getElementById("v").value;
+	var x = document.getElementById("x").value;
+	var t = document.getElementById("t").value;
+	var c = 3 * 10^8;
+	var tp = t-factorLorentz();
+	document.getElementById("tp").value = tp;
+}
+
+
+
+function dilatacion(){
+	
+}
