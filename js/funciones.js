3d36430dde846a057f1430249f330b4b8fa90f81
Julieth Cobos <jcobosg@unbosque.edu.co>
2017-03-05 09:45:55 -0500

    Actualización de formulas

--- js/funciones.js
+++ js/funciones.js
@@ -1,35 +1,36 @@
+//L
 function capturar(){
-    // obtenemos el valor de los elemento
-    var xb = document.getElementById("xb").value;
-    var xa = document.getElementById("xa").value;
-	resultado = parseInt(xb)+parseInt(xa);
-	document.getElementById("resultado").value = resultado;
+   var valorL = contraccionLongitudP()*factorLorentz();
+	document.getElementById("valorL").value = valorL;
 } 
 
-function factorLorentz (){
-	var v = document.getElementById("v").value;
-	var c = 3 * 10^8;
-	var k = Math.pow(1-(v^2/c^2));
+function factorLorentz(){
+	var v = parseFloat(document.getElementById("velocidad").value);
+	var c = 299792458;
+	var d = (Math.pow(v,2)*Math.pow(c,2))/Math.pow(c,2);
+	var k = Math.sqrt(Math.abs(1-d));
+	console.log(k);
 	return k;
 }
 
-function tiempo (){
+function tiempoP (){
 	var v = document.getElementById("v").value;
 	var x = document.getElementById("x").value;
 	var t = document.getElementById("t").value;
-	var c = 3 * 10^8;
-	var tp = t-factorLorentz();
+	var c = 299792458;
+	var num = t - (v/Math.pow(c,2))*x;
+	var tp = num/factorLorentz();
+
 	document.getElementById("tp").value = tp;
 }
 
 
-
-function contraccion(){
-	// obtenemos el valor de los elemento
-    var v = parseFloat(document.getElementById("v").value);
-	var c = 300000000;
-	var r = Math.abs(1-(v*v));
-	var k = Math.sqrt(r);
-	resultado = k;
-	document.getElementById("resultado").value = resultado;
+//L de prima
+function contraccionLongitudP(){
+	var xb = document.getElementById("xb").value;
+    var xa = document.getElementById("xa").value;
+	resultado = parseFloat(xb)-parseFloat(xa);
+	var valorLp = resultado/factorLorentz();
+	document.getElementById("valorLp").value = valorLp;
+	return valorLp;
 }
